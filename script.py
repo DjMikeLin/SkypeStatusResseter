e39b27d28f5ae5100a63f883674e9ca84e91a877
DjMikeLin <linmichael97@yahoo.com>
2021-04-23 23:10:57 -0400

    working

--- script.py
+++ script.py
@@ -1,36 +1,38 @@
 #!/usr/bin/env python3
 
+import sys
 import pyautogui
 import threading
-from pynput.keyboard import Listener
+import time
 from random import randint
 
 __author__ = "Michael(Yu) Lin"
 __version__ = "0.1.0"
 __licence__ = "MIT"
 
-def move_mouse():
+def move_mouse(threads_arr):
     random_X = randint(-60, 60)
     random_Y =  randint(-60, 60)
     random_Sec = randint(1, 60)
 
     pyautogui.move(random_X, random_Y)
     print("Mouse will move {} pixels horizontally and {} pixels vertically after {} seconds".format(random_X, random_Y, random_Sec)) 
-    threading.Timer(random_Sec, move_mouse).start()
-
-def kill_script(key):
-    #Kills script on 'ESC' button press
-    if key == keyboard.Key.esc:
-        print("Killing Script")
-        exit()
+    thread = threading.Timer(random_Sec, move_mouse(threads_arr))
+    threads_arr.append(thread)
+    #thread.daemon = True
+    thread.start()
 
 def main():
-    print("Starting script...")
+    try:
+        print("Starting script...")
 
-    with Listener(on_press=kill_script) as listener:
-        listener.join()
-    
-    move_mouse()
+        threads_arr = []
+        move_mouse(threads_arr)
+    except (KeyboardInterrupt, SystemExit):
+        for thr in threads_arr:
+            thr.join()
+
+        "Killing Script"
 
 if __name__ == "__main__":
     main()
