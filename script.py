f58756604df33bf44ac301696f380d267e6bea4d
DjMikeLin <linmichael97@yahoo.com>
2021-04-26 11:59:41 -0400

    1. Deleted redundant dependencies 2. Fixed issue where code was still being ran after timer is called

--- script.py
+++ script.py
@@ -1,38 +1,37 @@
 #!/usr/bin/env python3
 
-import sys
 import pyautogui
 import threading
-import time
 from random import randint
 
 __author__ = "Michael(Yu) Lin"
 __version__ = "0.1.0"
 __licence__ = "MIT"
 
-def move_mouse(threads_arr):
-    random_X = randint(-60, 60)
-    random_Y =  randint(-60, 60)
-    random_Sec = randint(1, 60)
+def move_timer(threads_arr):
+    while(True):
+        random_X = randint(-60, 60)
+        random_Y =  randint(-60, 60)
+        random_Sec = randint(1, 60)
+        
+        print("Mouse will move {} pixels horizontally and {} pixels vertically after {} seconds".format(random_X, random_Y, random_Sec)) 
+        
+        thread = threading.Timer(random_Sec, move_mouse, [random_X, random_Y])
+        thread.daemon = True
+        thread.start()
+        thread.join()
 
-    pyautogui.move(random_X, random_Y)
-    print("Mouse will move {} pixels horizontally and {} pixels vertically after {} seconds".format(random_X, random_Y, random_Sec)) 
-    thread = threading.Timer(random_Sec, move_mouse(threads_arr))
-    threads_arr.append(thread)
-    #thread.daemon = True
-    thread.start()
+def move_mouse(x, y):
+    pyautogui.move(x, y)
 
 def main():
     try:
         print("Starting script...")
 
         threads_arr = []
-        move_mouse(threads_arr)
-    except (KeyboardInterrupt, SystemExit):
-        for thr in threads_arr:
-            thr.join()
-
-        "Killing Script"
+        move_timer(threads_arr)
+    except(KeyboardInterrupt, SystemExit):
+        print("Killing Script")
 
 if __name__ == "__main__":
     main()
